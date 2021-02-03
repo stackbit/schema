246f06b30d13c12ccba0f3a9fadee816e8a79253
Simon Hanukaev <semenh@gmail.com>
2021-02-04 00:58:09 +0200

    added extendModels to main module index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 const modelUtils = require('./model-utils');
 const loadModels = require('./model-loader');
+const extendModels = require('./model-extender');
 const { getModelByQuery, getModelsByQuery } = require('./model-matcher');
 
 
 module.exports = {
     loadModels,
+    extendModels,
     getModelByQuery,
     getModelsByQuery,
     ...modelUtils
